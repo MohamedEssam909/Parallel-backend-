48565d20b5ef356ace7147e42798b122c68b16a4
Mohamed Essam <146870047+MohamedEssam909@users.noreply.github.com>
2025-05-08 18:40:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/images', express.static(__dirname + '/images'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 
